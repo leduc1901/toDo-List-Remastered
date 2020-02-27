2b1c6313e2d2281ae5c11a2a491c79c33f9f937e
leduc1901 <leduc1901@github.com>
2020-02-27 11:22:17 +0700

    Updates

---
+++ precache-manifest.63fe2edff2d2dceadde1b90faf7a56ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98b201b274831c91b141485aef7695d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "abb4e0562c1da1506d8a",
+    "url": "/todo-list/static/css/main.f2c17dfb.chunk.css"
+  },
+  {
+    "revision": "49d07684db3bac8de447",
+    "url": "/todo-list/static/js/2.328a9ef5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-list/static/js/2.328a9ef5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abb4e0562c1da1506d8a",
+    "url": "/todo-list/static/js/main.e1742876.chunk.js"
+  },
+  {
+    "revision": "e2438a87e2ac536845e5",
+    "url": "/todo-list/static/js/runtime-main.312c004b.js"
+  }
+]);
